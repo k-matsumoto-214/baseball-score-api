90e0588dca75b93fc1bc57febbc146a0e781ab50
keisuke-nao-baseball <85728967+keisuke-nao-baseball@users.noreply.github.com>
2021-07-22 15:10:46 +0900

    Team (#3)
    * ログイン機能追加
    
    * ログイン認証の確認
    
    * コメント修正
    
    * コメント修正
    
    * gradle修正
    
    * entity修正
    
    * playersDao作成
    
    * playerService完成
    
    * コントローラー完成
    
    * playerService動作確認　修正
    
    * URL修正
    
    * URL修正
    
    * 一部サービスの修正
    
    * チーム管理API実装

---
+++ score/src/main/java/basaball/score/dao/TeamsDao.java
@@ -0,0 +1,70 @@
+package basaball.score.dao;
+
+import basaball.score.entity.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TeamsDao {
+  @Autowired
+  private NamedParameterJdbcTemplate jdbcTemplate;
+
+  public int create(Team team) {
+    String sql = "insert into teams values (null, :name, :comment, :image, :accountId, :password)";
+
+    SqlParameterSource parameters = new MapSqlParameterSource("name", team.getName())
+                                        .addValue("comment", team.getComment())
+                                        .addValue("image", team.getImage())
+                                        .addValue("accountId", team.getAccountId())
+                                        .addValue("password", team.getPassword());
+    return jdbcTemplate.update(sql, parameters);
+  }
+
+  public Team findById(int id) {
+    String sql = "select * from teams where id = :id";
+
+    SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+
+    RowMapper<Team> rowMapper = new BeanPropertyRowMapper<Team>(Team.class);
+
+    try {
+      return jdbcTemplate.queryForObject(sql, parameters, rowMapper);
+    } catch (EmptyResultDataAccessException e) {
+      return null;
+    }
+  }
+
+  public void selectForUpdate(int id) {
+    String sql = "select * from teams where id = :id for update";
+
+    SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+
+    jdbcTemplate.queryForMap(sql, parameters);
+  }
+
+  public int update(Team team) {
+    String sql = "update teams set name = :name, comment = :comment, image = :image "
+                 + "where id = :id";
+
+    SqlParameterSource parameters = new MapSqlParameterSource("name", team.getName())
+                                        .addValue("comment", team.getComment())
+                                        .addValue("image", team.getImage())
+                                        .addValue("id", team.getId());
+
+    return jdbcTemplate.update(sql, parameters);
+  }
+
+  public int delete(int id) {
+    String sql = "delete from teams where id = :id";
+
+    SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+
+    return jdbcTemplate.update(sql, parameters);
+  }
+}
---
+++ score/src/main/java/basaball/score/form/TeamForm.java
@@ -0,0 +1,19 @@
+package basaball.score.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class TeamForm {
+  @Size(min = 1, max = 100)
+  private String accountId;
+  @Size(min = 8, max = 100)
+  private String password;
+  @Size(min = 1, max = 100)
+  @NotBlank
+  private String name;
+  @Size(max = 300)
+  private String comment;
+  private String image;
+}
