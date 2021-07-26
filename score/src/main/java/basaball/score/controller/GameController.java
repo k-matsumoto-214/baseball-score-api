e849042dfc8a29b3b0594e5ce022d90db17d964b
keisuke-nao-baseball <matsukei7539@gmail.com>
2021-07-26 13:35:50 +0900

    試合更新機能の追加

---
+++ score/src/main/java/basaball/score/controller/GameController.java
@@ -0,0 +1,66 @@
+package basaball.score.controller;
+
+import basaball.score.controller.exception.DataNotFoundException;
+import basaball.score.controller.exception.RegistrationException;
+import basaball.score.controller.exception.UpdateException;
+import basaball.score.entity.Game;
+import basaball.score.form.GameForm;
+import basaball.score.security.LoginTeam;
+import basaball.score.service.GameService;
+import basaball.score.service.UtilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController {
+  @Autowired
+  private UtilService utilService;
+  @Autowired
+  private GameService gameService;
+
+  @GetMapping("/games")
+  public ResponseEntity<Object> fetchGames(@AuthenticationPrincipal LoginTeam team) throws DataNotFoundException {
+    return utilService.responseFromObject(gameService.findByTeamId(team.getId()));
+  }
+
+  @PostMapping("/games")
+  public ResponseEntity<Object> createGame(
+      @AuthenticationPrincipal LoginTeam team, @RequestBody GameForm form) throws DataNotFoundException, RegistrationException {
+    Game game = new Game();
+    game.setTeamId(team.getId());
+    game.setOpponentTeam(form.getOpponentTeam());
+    game.setDate(form.getDate());
+    game.setField(form.getField());
+    game.setTopFlg(form.isTopFlg());
+    return utilService.responseFromObject(gameService.create(game));
+  }
+
+  @GetMapping("/games/{gameId}")
+  public ResponseEntity<Object> fetchGame(@AuthenticationPrincipal LoginTeam team, @PathVariable int gameId) throws DataNotFoundException {
+    return utilService.responseFromObject(gameService.findById(gameId, team.getId()));
+  }
+
+  @PostMapping("games/{gameId}")
+  public ResponseEntity<Object> updateGame(@AuthenticationPrincipal LoginTeam team, @RequestBody GameForm form, @PathVariable int gameId) throws UpdateException {
+    Game game = new Game();
+    game.setId(gameId);
+    game.setTeamId(team.getId());
+    game.setTopScore(form.getTopScore());
+    game.setBottomScore(form.getBottomScore());
+    game.setOpponentTeam(form.getOpponentTeam());
+    game.setDate(form.getDate());
+    game.setField(form.getField());
+    game.setResult(form.getResult());
+    game.setTopFlg(form.isTopFlg());
+    game.setResultFlg(form.isResultFlg());
+    game.setLineupingStatus(form.getLineupingStatus());
+    gameService.update(game);
+    return utilService.response();
+  }
+}
---
+++ score/src/main/java/basaball/score/service/GameService.java
@@ -0,0 +1,90 @@
+package basaball.score.service;
+
+import basaball.score.controller.exception.DataNotFoundException;
+import basaball.score.controller.exception.RegistrationException;
+import basaball.score.controller.exception.UpdateException;
+import basaball.score.dao.GamesDao;
+import basaball.score.entity.Game;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GameService {
+  @Autowired
+  private GamesDao gamesDao;
+
+  public Map<String, Object> create(Game game) throws RegistrationException {
+    if (gamesDao.create(game) != 1) {
+      throw new RegistrationException("試合登録に失敗しました。");
+    }
+    int id = gamesDao.fetchLastInsertId();
+    Map<String, Object> result = new LinkedHashMap<>();
+    result.put("id", id);
+    return result;
+  }
+
+  public List<Map<String, Object>> findByTeamId(int teamId) throws DataNotFoundException {
+    List<Game> games = gamesDao.findByTeamId(teamId);
+    if (games == null) {
+      throw new DataNotFoundException("試合情報が見つかりません。");
+    }
+
+    List<Map<String, Object>> result = new ArrayList<>();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    for (Game game : games) {
+      Map<String, Object> tempMap = new LinkedHashMap<>();
+      tempMap.put("id", game.getId());
+      tempMap.put("teamId", game.getTeamId());
+      tempMap.put("opponentTeam", game.getOpponentTeam());
+      tempMap.put("topScore", game.getTopScore());
+      tempMap.put("bottomScore", game.getBottomScore());
+      tempMap.put("date", game.getDate() != null ? sdf.format(game.getDate()) : null);
+      tempMap.put("field", game.getField());
+      tempMap.put("result", game.getResult());
+      tempMap.put("topFlg", game.isTopFlg());
+      tempMap.put("resultFlg", game.isResultFlg());
+      tempMap.put("lineupingStatus", game.getLineupingStatus());
+      result.add(tempMap);
+    }
+
+    return result;
+  }
+
+  public Map<String, Object> findById(int gameId, int teamId) throws DataNotFoundException {
+    Game game = gamesDao.findById(gameId, teamId);
+    if (game == null) {
+      throw new DataNotFoundException("試合情報が見つかりません。");
+    }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Map<String, Object> result = new LinkedHashMap<>();
+    result.put("id", game.getId());
+    result.put("teamId", game.getTeamId());
+    result.put("opponentTeam", game.getOpponentTeam());
+    result.put("topScore", game.getTopScore());
+    result.put("bottomScore", game.getBottomScore());
+    result.put("date", game.getDate() != null ? sdf.format(game.getDate()) : null);
+    result.put("field", game.getField());
+    result.put("result", game.getResult());
+    result.put("topFlg", game.isTopFlg());
+    result.put("resultFlg", game.isResultFlg());
+    result.put("lineupingStatus", game.getLineupingStatus());
+
+    return result;
+  }
+
+  @Transactional(rollbackFor = Exception.class)
+  public void update(Game game) throws UpdateException {
+    gamesDao.selectForUpdate(game.getId());
+    if (gamesDao.update(game) != 1) {
+      throw new UpdateException("試合情報更新に失敗しました。");
+    }
+  }
+}
