e04049a24b2daae31088a578b3ba425ac80374da
keisuke-nao-baseball <85728967+keisuke-nao-baseball@users.noreply.github.com>
2021-08-26 10:02:41 +0900

    ひとつ前に戻った時の選手交代を反映 (#14)

--- score/src/main/java/basaball/score/service/AtBatService.java
+++ score/src/main/java/basaball/score/service/AtBatService.java
@@ -7,12 +7,19 @@ import basaball.score.dao.AtBatsDao;
 import basaball.score.dao.BatteryErrorsDao;
 import basaball.score.dao.ErrorsDao;
 import basaball.score.dao.EventsDao;
+import basaball.score.dao.GamesDao;
+import basaball.score.dao.PlayerChangeDao;
 import basaball.score.dao.RunOutsDao;
 import basaball.score.dao.RunsDao;
 import basaball.score.dao.SpecialsDao;
 import basaball.score.dao.StealsDao;
 import basaball.score.entity.AtBat;
 import basaball.score.entity.Event;
+import basaball.score.entity.Game;
+import basaball.score.entity.PlayerChange;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -39,6 +46,10 @@ public class AtBatService {
   private StealsDao stealsDao;
   @Autowired
   private SpecialsDao specialsDao;
+  @Autowired
+  private PlayerChangeDao playerChangeDao;
+  @Autowired
+  private GamesDao gamesDao;
 
   public List<Map<String, Object>> findByGameId(int gameId) throws DataNotFoundException {
     List<AtBat> atBats = atBatsDao.findByGameId(gameId);
@@ -106,6 +117,49 @@ public class AtBatService {
                 break;
               case 3:
                 specialsDao.deleteByEventId(event.getId());
+                break;
+              case 4:
+                // オーダーのリセット
+                Game target = gamesDao.findById(event.getGameId(), event.getTeamId());
+                JsonArray targetLineups = target.isTopFlg()
+                                              ? JsonParser.parseString(target.getTopLineup()).getAsJsonArray()
+                                              : JsonParser.parseString(target.getBottomLineup()).getAsJsonArray();
+
+                for (int i = 0; i < targetLineups.size(); i++) {
+                  int deleteIdx = (targetLineups.get(i).getAsJsonObject().get("orderDetails").getAsJsonArray().size()) - 1;
+                  targetLineups.get(i).getAsJsonObject().get("orderDetails").getAsJsonArray().remove(deleteIdx);
+                }
+
+                if (target.isTopFlg()) {
+                  target.setTopLineup(targetLineups.toString());
+                } else {
+                  target.setBottomLineup(targetLineups.toString());
+                }
+                gamesDao.selectForUpdate(event.getGameId());
+                gamesDao.update(target);
+
+                List<PlayerChange> playerChanges = playerChangeDao.findByEventId(event.getId(), event.getTeamId());
+                // 選手交代イベントが投手交代、代打の場合は打席情報のリセット
+                for (PlayerChange playerChange : playerChanges) {
+                  if (playerChange.getChangeStatus() == 2) {
+                    beforeAtBat.setBatterId(playerChange.getOutPlayerId());
+                    break;
+                  }
+                  if (playerChange.getChangeStatus() == 0) {
+                    for (JsonElement targetLineup : targetLineups) {
+                      int lastIdx = (targetLineup.getAsJsonObject().get("orderDetails").getAsJsonArray().size()) - 1;
+                      int fieldNumber = targetLineup.getAsJsonObject().get("orderDetails").getAsJsonArray().get(lastIdx).getAsJsonObject().get("fieldNumber").getAsInt();
+                      if (fieldNumber == 1) {
+                        beforeAtBat.setPitcherId(targetLineup.getAsJsonObject().get("orderDetails").getAsJsonArray().get(lastIdx).getAsJsonObject().get("playerId").getAsInt());
+                        break;
+                      }
+                    }
+                    break;
+                  }
+                }
+
+                // 選手交代イベントの削除
+                playerChangeDao.deleteByEventId(event.getId());
                 break;
             }
           }
